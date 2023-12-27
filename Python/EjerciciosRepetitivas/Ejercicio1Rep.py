1caa8b75b71f8b58e1e289e853ea212c1d38b38f
FranSantana0522 <franciscojaviermontes2014@gmail.com>
2023-12-27 16:16:37 +0100

    Repetitivas

---
+++ Python/EjerciciosRepetitivas/Ejercicio1Rep.py
@@ -0,0 +1,13 @@
+num = int(input("Introduce numero: "))
+i = 0
+while i<11:
+    print(num," x ", i, " = ",num*i) 
+    i+=1
+else:
+    print("Bucle while\n")
+
+for j in range(11):
+    print(num," x ", j, " = ",num*j) 
+else:
+    print("Bucle for\n")
+
