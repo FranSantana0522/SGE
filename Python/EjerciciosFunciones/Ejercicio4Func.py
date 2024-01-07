51d1c02d8bd7e3dad5a01be73c1ff1f5e1a4417e
FranSantana0522 <franciscojaviermontes2014@gmail.com>
2024-01-07 14:36:36 +0100

    Funciones

--- Python/EjerciciosFunciones/Ejercicio4Func.py
+++ Python/EjerciciosFunciones/Ejercicio4Func.py
@@ -4,6 +4,17 @@ def guardarAgenda(l_agenda,**kwargs):
     resultado.append(kwargs)
     return resultado
 
+def buscar(l_agenda,**kwargs):
+    listaAciertos = []
+    for contacto in l_agenda:
+        aciertos=0
+        for campo,valor in kwargs.items():
+            if campo in contacto and contacto[campo] == valor:
+                aciertos+=1
+        if aciertos==len(kwargs):
+            listaAciertos.append(contacto)
+    return listaAciertos
+
 def main():
     agenda = []
     cantidad=int(input("Cantidad de contactos a agendar: "))
@@ -17,6 +28,13 @@ def main():
             campo=input("Introduzca otro campo de la agenda: ")
         agenda=guardarAgenda(agenda,**contacto)[:]
     print(agenda)
+
+    filtro={}
+    campo=input("Introduca un campo para buscar: ")
+    while campo !="*":
+        filtro[campo]= input("Introduzca el valor: ")
+        campo=input("Introduca un campo para buscar: ")
+    print(buscar(agenda,**filtro))
 
 if __name__=='__main__':
     main()
